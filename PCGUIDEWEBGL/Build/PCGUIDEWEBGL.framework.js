2567fe5b1f6aa57a0ef1313c3e15f1381a7acb5e
chubeather <case_2426@hotmail.com>
2021-04-02 11:03:06 +0700

    Add files via upload

--- PCGUIDEWEBGL/Build/PCGUIDEWEBGL.framework.js
+++ PCGUIDEWEBGL/Build/PCGUIDEWEBGL.framework.js
@@ -1298,7 +1298,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2235360;
+STATICTOP = STATIC_BASE + 2236064;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3284,7 +3284,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2235360;
+var STATIC_BUMP = 2236064;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15215,8 +15215,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 50884;
-Module["wasmMaxTableSize"] = 50884;
+Module["wasmTableSize"] = 50876;
+Module["wasmMaxTableSize"] = 50876;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
